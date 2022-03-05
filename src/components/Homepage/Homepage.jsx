e229d515e8cecb54b11cc11c7f230d2980ffd962
Yegor Yefimov <egoryefim@gmail.com>
2022-03-05 09:11:18 +0200

    created HomePage component

---
+++ src/components/Homepage/Homepage.jsx
@@ -0,0 +1,38 @@
+const HomePage = () => (
+    <div className="HomePage">
+        <div className="HomePage-menu">
+            <div className="HomePage-menu__item">
+                <div className="HomePage-content">
+                    <h1 className="HomePage-title">HATS</h1>
+                    <span className="HomePage-subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="HomePage-menu__item">
+                <div className="HomePage-content">
+                    <h1 className="HomePage-title">JACKETS</h1>
+                    <span className="HomePage-subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="HomePage-menu__item">
+                <div className="HomePage-content">
+                    <h1 className="HomePage-title">SNEAKERS</h1>
+                    <span className="HomePage-subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="HomePage-menu__item">
+                <div className="HomePage-content">
+                    <h1 className="HomePage-title">WOMENS</h1>
+                    <span className="HomePage-subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="HomePage-menu__item">
+                <div className="HomePage-content">
+                    <h1 className="HomePage-title">MENS</h1>
+                    <span className="HomePage-subtitle">SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
