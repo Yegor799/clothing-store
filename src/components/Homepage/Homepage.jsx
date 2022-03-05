be518dc1e116aacdde8f508c3ac17441285dbe60
Yegor Yefimov <egoryefim@gmail.com>
2022-03-05 11:53:09 +0200

    added styles for homepage

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,34 +1,36 @@
+import './Homepage.scss';
+
 const HomePage = () => (
-    <div className="HomePage">
-        <div className="HomePage-menu">
-            <div className="HomePage-menu__item">
-                <div className="HomePage-content">
-                    <h1 className="HomePage-title">HATS</h1>
-                    <span className="HomePage-subtitle">SHOP NOW</span>
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">HATS</h1>
+                    <span className="subtitle">SHOP NOW</span>
                 </div>
             </div>
-            <div className="HomePage-menu__item">
-                <div className="HomePage-content">
-                    <h1 className="HomePage-title">JACKETS</h1>
-                    <span className="HomePage-subtitle">SHOP NOW</span>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">JACKETS</h1>
+                    <span className="subtitle">SHOP NOW</span>
                 </div>
             </div>
-            <div className="HomePage-menu__item">
-                <div className="HomePage-content">
-                    <h1 className="HomePage-title">SNEAKERS</h1>
-                    <span className="HomePage-subtitle">SHOP NOW</span>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">SNEAKERS</h1>
+                    <span className="subtitle">SHOP NOW</span>
                 </div>
             </div>
-            <div className="HomePage-menu__item">
-                <div className="HomePage-content">
-                    <h1 className="HomePage-title">WOMENS</h1>
-                    <span className="HomePage-subtitle">SHOP NOW</span>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">WOMENS</h1>
+                    <span className="subtitle">SHOP NOW</span>
                 </div>
             </div>
-            <div className="HomePage-menu__item">
-                <div className="HomePage-content">
-                    <h1 className="HomePage-title">MENS</h1>
-                    <span className="HomePage-subtitle">SHOP NOW</span>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">MENS</h1>
+                    <span className="subtitle">SHOP NOW</span>
                 </div>
             </div>
         </div>
